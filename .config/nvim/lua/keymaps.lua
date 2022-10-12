36988fcb09eeaa8c31f2e24123bf8a489954df27
Marcelo Risoli <celorisoli@gmail.com>
2022-10-12 11:06:37 +0200

    add leap keymaps

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -6,4 +6,10 @@ vim.api.nvim_set_keymap('n', '<leader>g', ':Rg<cr>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-k>', '<Plug>(ale_previous_wrap)', { noremap = false, silent = true })
 vim.api.nvim_set_keymap('n', '<C-j>', '<Plug>(ale_next_wrap)', { noremap = false, silent = true })
 
-vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>', { noremap = true, silent = true })
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', 'ff', builtin.find_files, {})
+vim.keymap.set('n', 'fg', builtin.live_grep, {})
+vim.keymap.set('n', 'fb', builtin.buffers, {})
+
+vim.keymap.set('n', '-', '<Plug>(leap-forward)', {})
+vim.keymap.set('n', '_', '<Plug>(leap-backward)', {})
