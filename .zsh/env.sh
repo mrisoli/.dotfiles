e7bca1ada48e4e2c6c2233f3a67d91b617d0c990
Marcelo Risoli <celorisoli@gmail.com>
2024-09-09 16:22:05 +0300

    update zsh files

--- .zsh/env.sh
+++ .zsh/env.sh
@@ -4,6 +4,9 @@ alias work="cd $HOME/workspace"
 alias n="nvim"
 alias t='todo.sh -d ~/todo.cfg'
 alias pn="pnpm"
-alias nuke="rm -rf node_modules && bun install"
+alias nukey="rm -rf node_modules && yarn"
+alias nukeb="rm -rf node_modules && bun install"
+
 . "$HOME/.cargo/env"
+
 export HOMEBREW_AUTO_UPDATE_SECS=86400
--- .zsh/pomo.sh
+++ .zsh/pomo.sh
@@ -3,11 +3,6 @@ alias wo="timer 25m && terminal-notifier -message 'Pomodoro'\
         -appIcon '~/Pictures/pumpkin.png'\
         -sound Crystal"
 
-alias br="timer 5s && terminal-notifier -message 'Pomodoro'\
-        -title 'Break is over! Get back to work 😬'\
-        -appIcon '~/Pictures/pumpkin.png'\
-        -sound Crystal"
-
 alias re="timer 5m && terminal-notifier -message 'Pomodoro'\
         -title 'Break is over! Get back to work 🧑🏻‍💻'\
         -appIcon '~/Pictures/pumpkin.png'\
