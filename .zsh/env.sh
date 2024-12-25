a12e4e5221090936d82a170c6c9313be9195b051
Marcelo Risoli <celorisoli@gmail.com>
2024-12-25 18:58:09 -0300

    update zsh files

--- .zsh/env.sh
+++ .zsh/env.sh
@@ -4,9 +4,7 @@ alias work="cd $HOME/workspace"
 alias n="nvim"
 alias t='todo.sh -d ~/todo.cfg'
 alias pn="pnpm"
-alias nukey="rm -rf node_modules && yarn"
-alias nukeb="rm -rf node_modules && bun install"
-
+alias nuke="rm -rf node_modules && yarn"
+alias gbclean="git branch | grep -v main | xargs git branch -D"
 . "$HOME/.cargo/env"
-
 export HOMEBREW_AUTO_UPDATE_SECS=86400
